1b635f501c5952696fcc2a9ea351e81358fc9451
nikolay <saitovkms@gmail.com>
2021-09-29 20:21:06 +0300

    15th

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+"use strict";
+/*
+//12. Практика, ч.1. Начинаем создавать приложение
 const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', ''); 
 
 const personalMovieDB = {
@@ -17,3 +20,72 @@ personalMovieDB.movies[a] = b;
 personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+//13. Условия
+
+if(4 == 6){
+    console.log('Ok!');
+
+}else {
+    console.log('Error');
+}
+
+const num = 50;
+
+if (num<49) {
+    console.log('error');
+
+} else if (num>100) {
+    console.log(' Много');
+} else {
+    console.log(' ok');
+}
+
+(num === 50) ? console.log(' ok') : console.log('error');
+
+
+//const num = 50;
+switch(num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;    
+    case 50: 
+        console.log('В точку');
+        break;
+    default: 
+        console.log('Не в этот раз');
+        break;
+}
+
+
+
+//14. Циклы
+
+let num = 50;
+
+while (num<55) {
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+while (num<545);
+
+for (let i = 1; i<10; i++) {
+
+    if (i===6){
+        //break;
+        continue;
+    }
+    console.log(i);
+    
+}*/
+
+//15. Практика, ч2. Применяем условия и циклы
+
